1b5fe829e06e05d667d1dd14af599d6ceb0e4252
Daniel Castro <dcastro9@gatech.edu>
2015-01-22 23:34:27 -0500

    Adding code to perform the Lucas-Lehmer test, found online.

--- find_primes.py
+++ find_primes.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def findPrimes(max_prime):
     """ This function returns the primes between 0 and max.
 
@@ -39,10 +39,50 @@ def findPrimes(max_prime):
             break
         else:
             length_of_numbers.append(len(numbers))
-    print "Iterations: " + str(iterations)
-    print "Length of numbers: " + str(length_of_numbers)
-    print "Prime Numbers between 1 and " + str(max_prime) + ":"
-    print numbers
     return numbers
 
-findPrimes(100000)
+# Code below from http://rosettacode.org/wiki/Lucas-Lehmer_test#Python
+def isqrt(n):
+    if n < 0:
+        raise ValueError
+    elif n < 2:
+        return n
+    else:
+        a = 1 << ((1 + n.bit_length()) >> 1)
+        while True:
+            b = (a + n // a) >> 1
+            if b >= a:
+                return a
+            a = b
+ 
+def isprime(n):
+    if n < 5:
+        return n == 2 or n == 3
+    elif n%2 == 0:
+        return False
+    else:
+        r = isqrt(n)
+        k = 3
+        while k <= r:
+            if n%k == 0:
+                return False
+            k += 2
+        return True
+ 
+def lucas_lehmer_fast(n):
+    if n == 2:
+        return True
+    elif not isprime(n):
+        return False
+    else:
+        m = 2**n - 1
+        s = 4
+        for i in range(2, n):
+            sqr = s*s
+            r = (sqr & m) + (sqr >> n)
+            if r >= m:
+                r -= m
+            s = r - 2
+        return s == 0
+# Code above is taken from http://rosettacode.org/wiki/Lucas-Lehmer_test#Python
+
